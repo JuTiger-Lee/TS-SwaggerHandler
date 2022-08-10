8cc03cc6fb58a1c456e850fceedc17ad9b5ac221
Jeffey <ref6318@gmail.com>
2022-08-10 23:01:39 +0900

    [FIX]: user API document 추가

--- src/controllers/apiDocs/ApiDocs.ts
+++ src/controllers/apiDocs/ApiDocs.ts
@@ -1,13 +1,16 @@
 import swaggerUI from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 import SwaggerHandler from "@/handler/SwaggerHandler";
+import userAPI from "@/controllers/apiDocs/user/index";
 
 class ApiDocs {
   private apiDocOption: object;
   private swagger: SwaggerHandler;
 
   constructor() {
-    this.apiDocOption = {};
+    this.apiDocOption = {
+      ...userAPI,
+    };
 
     this.swagger = SwaggerHandler.getSwaggerInstance();
   }
