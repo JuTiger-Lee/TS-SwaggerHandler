dbe6de4439623d713916b21a45fc1d3a159a6525
Jeffey <ref6318@gmail.com>
2022-08-10 23:00:12 +0900

    [FIX]: user router 추가

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import userRouter from "@/routers/user";
 
 export default (app: express.Application) => {
   app.get("/", (req, res) => res.send("Hello World"));
+  app.use("/api/user", userRouter);
 };
